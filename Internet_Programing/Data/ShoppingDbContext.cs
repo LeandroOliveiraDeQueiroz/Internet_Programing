7d96c89fba10fc40d827f563debf03a446177dc1
Leandro Oliveira <leandroliveiradequeiroz@gmail.com>
2020-11-28 21:50:24 +0000

    Product CRUD - create database

---
+++ Internet_Programing/Data/ShoppingDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Internet_Programing.Data
+{
+    public class ShoppingDbContext : DbContext
+    {
+        public ShoppingDbContext(DbContextOptions<ShoppingDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Internet_Programing.Models.Products> Product { get; set; }
+
+
+    }
+}
