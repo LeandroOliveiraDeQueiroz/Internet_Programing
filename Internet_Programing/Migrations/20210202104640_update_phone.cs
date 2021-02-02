d965af4bbce59a3a461cf6842ff7d092410b7f0f
Leandro Oliveira <leandroliveiradequeiroz@gmail.com>
2021-02-02 11:10:04 +0000

    Change type price of float to decimal

---
+++ Internet_Programing/Migrations/20210202104640_update_phone.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Internet_Programing.Migrations
+{
+    public partial class update_phone : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Phone",
+                type: "decimal(18,2)",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Phone",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,2)");
+        }
+    }
+}
--- Internet_Programing/Models/Phone.cs
+++ Internet_Programing/Models/Phone.cs
@@ -8,19 +8,24 @@ namespace Internet_Programing.Models
 {
     public class Phone
     {
+        //private const decimal V = 1.0m;
+
         public int Id { set; get; }
         [Required]
         [StringLength(256)]
         public string Name { set; get; }
         [Required]
-        [Range(0, int.MaxValue, ErrorMessage = "Price need to be positive number.")]
-        public float Price { set; get; }
+        [Range(0.1, Double.MaxValue, ErrorMessage = "Price need to be positive number.")]
+        public decimal Price { set; get; }
         public string Description { set; get; }
         public int OSId { set; get; }
         public OS OS { set; get; }
+        [Range(1, int.MaxValue, ErrorMessage = "Battery Ampere need to be positive number.")]
         public int BatteryAmpere { set; get; }
+        [Range(1, int.MaxValue, ErrorMessage = "RAM need to be positive number.")]
         public int RAM { set; get; }
-        public int Memory { set; get; } //GB
+        [Range(1, int.MaxValue, ErrorMessage = "Memory need to be positive number.")]
+        public int Memory { set; get; }
         public string Processor { set; get; }
 
         public ICollection<CartPhone> Cart { get; set; }
